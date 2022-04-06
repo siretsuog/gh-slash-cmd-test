24fe40fd0e701159105542e27c6c5e75a4b55a14
siretsuog <solon_gousteris@outlook.com.gr>
2022-04-06 12:02:30 +0000

    dummy

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -euo pipefail
-
+# dummy
 user="$1"
 
 user=$(curl -1sLf -H "authorization: Bearer $GITHUB_API_TOKEN" -H "Accept: application/vnd.github.v3+json" https://api.github.com/orgs/redpanda-data/members?per_page=1000 | jq -r ".[] | select(.login==\"$user\")")
